935ae26f92b663f708985d205ab05fdc6838ba11
Pragati Patel <pragatinpatel@gmail.com>
2023-03-25 09:23:11 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = "go to the office" ) {
+    return `This Monday, I will ${activity}.`;
+}
+/*
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+*/
+
+function wrapAdjective(wrapper='*') {
+    return function inner(adjetive='special') {
+      return `You are ${wrapper}${adjetive}${wrapper}!`
+    }
+  }
